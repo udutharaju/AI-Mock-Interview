6a6d7af5e144ea440fe83b587d9504c1e2b70bf4
udutharaju <rajuudutha9963@gmail.com>
2025-06-18 08:22:46 +0530

    project setup completed

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+
+
+const App = () => {
+  return (
+       <div className='text-blue-500'>
+
+       <Button variant={"destructive"} size = {"icon"}>hi </Button>        
+        </div>
+   
+    
+   
+  );
+};
+
+export default App;
